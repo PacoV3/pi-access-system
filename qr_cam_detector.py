fa76e33619dda671a3c8548be55b0696080182e0
PacoV3 <franj.vents@gmail.com>
2021-03-24 21:46:58 -0600

    QR and Temp Sensor Code

---
+++ qr_cam_detector.py
@@ -0,0 +1,12 @@
+class QRCamDetector:
+	def __init__(self,cam,detector):
+		self.cam = cam
+		self.detector = detector
+	
+	# QR Detection
+	def qr_data_box(self):
+		# Get image
+		_, img = self.cam.read()
+		# Get bounding box and data
+		data, bbox, _ = self.detector.detectAndDecode(img)
+		return data, bbox
---
+++ temp_sensor.py
@@ -0,0 +1,8 @@
+from mlx90614 import MLX90614
+
+class TemperatureSensor:
+	def __init__(self,bus):
+		self.sensor = MLX90614(bus, address=0x5A)
+	
+	def body_temperature(self):
+		return self.sensor.get_object_1()
